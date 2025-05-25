3608b8776205ffdc6de368981528f3a956519152
Daniel <Daniel@DANIELRF>
2025-05-24 23:33:28 -0600

    Add project files.

---
+++ MCP_SQL/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Headers;
+using MCP_SQL;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddMcpServer()
+    .WithStdioServerTransport()
+    .WithTools<SQLTools>();
+
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+await builder.Build().RunAsync();
---
+++ MCP_SQL/SQLTools.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Data.SqlClient;
+using System.ComponentModel;
+using System.Text;
+using ModelContextProtocol.Server;
+
+namespace MCP_SQL
+{
+    [McpServerToolType]
+    public sealed class SQLTools
+    {
+        [McpServerTool, Description("Get weather forecast for a City.")]
+        public static async Task<string> ExecuteSql(
+            [Description("SQL query")] string sql,
+            [Description("connectionString of the SQL server")] string _connectionString
+            )
+        {
+            using var connection = new SqlConnection(_connectionString);
+            using var command = new SqlCommand(sql, connection);
+
+            var resultBuilder = new StringBuilder();
+
+            await connection.OpenAsync();
+
+            using var reader = await command.ExecuteReaderAsync();
+
+            while (await reader.ReadAsync())
+            {
+                for (int i = 0; i < reader.FieldCount; i++)
+                {
+                    resultBuilder.Append($"{reader.GetName(i)}: {reader[i]}");
+                    if (i < reader.FieldCount - 1)
+                        resultBuilder.Append(" | ");
+                }
+                resultBuilder.AppendLine();
+            }
+
+            return resultBuilder.ToString();
+
+        }
+    }
+}
